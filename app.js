3030057a1a14128df094f2a5470d25000a425cbd
Your Name <you@example.com>
2024-09-10 17:35:27 +0530

    new

--- app.js
+++ app.js
@@ -1,38 +1,85 @@
-/*fetch(" https://restcountries.com/v3.1/all")
-.then(res =>res.json())
-.then(data =>{
-    let tblCountry=document.getElementById("tblCountry");
-    
-    let tblBody=`<tr>
-                    <th>Country Name</th>
-                    <th>Flag</th>
-                 </tr>`;
-    
-    data.forEach(element => {
-        tblBody+=`<tr>
-                     <td>${element.name.common}</td>
-                     <td>${element.flag}</td>
-                  </tr>`
-    });
-    tblCountry.innerHTML=tblBody;
-})*/
+const map = L.map('map').setView([0, 0], 2); // Start with zoomed-out map
 
-/*function searchCountry(){
-    let search=document.getElementById("txtSeaarch").value
-    fetch(`https://restcountries.com/v3.1/name/${search}`)
-    .then(res => res.json())
-    .then(data=>{
-        data.forEach(element => {
-            document.getElementById("name").innerHTML=element.name.official
-            document.getElementById("img").innerHTML=`<img src="${element.flags.png}" alt="">`
-          
+// Add the OpenStreetMap tiles layer (the map imagery)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 
-        });
-    })
+// Check if geolocation is available
+if (navigator.geolocation) {
+    // Request the user's location
+    navigator.geolocation.getCurrentPosition(showPositionAuto, showError)
+
+} else {
+    document.getElementById("location-details").textContent = "Geolocation is not supported by this browser.";
 }
-findLocation();
 
-document.getElementById("map").innerHTML = <div id="googleMap" style="width:100%;height:800px;"></div>;*/
+// Function to display user's location on the map
+function showPositionAuto(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    // Set the view to the user's location with a suitable zoom level
+    map.setView([lat, lon], 13);
+
+    // Add a marker at the user's location
+    L.marker([lat, lon]).addTo(map)
+        .bindPopup("You are here!")
+        .openPopup();
+
+    // Fetch location name using reverse geocoding from Nominatim
+    getLocationName(lat, lon);
+}
+
+// Function to get location name using Nominatim API
+function getLocationName(lat, lon) {
+    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=10&addressdetails=1`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const locationDetails = data.address;
+            let locationName = '';
+
+            // Construct location name (you can adjust this as needed)
+            if (locationDetails.city) {
+                locationName=locationDetails.city
+                findWeatherAuto(locationName)
+                forecastAuto(locationName)
+                locationName += ','
+            }
+            if (locationDetails.state) {
+                locationName += locationDetails.state + ', ';
+            }
+            if (locationDetails.country) {
+                locationName += locationDetails.country;
+            }
+
+            // Show location name on the page
+            document.getElementById("location-details").textContent = "Location: " + locationName;
+        })
+        .catch(error => {
+            document.getElementById("location-details").textContent = "Unable to retrieve location name.";
+        });
+}
+
+// Handle possible errors
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            document.getElementById("location-details").textContent = "User denied the request for Geolocation.";
+            break;
+        case error.POSITION_UNAVAILABLE:
+            document.getElementById("location-details").textContent = "Location information is unavailable.";
+            break;
+        case error.TIMEOUT:
+            document.getElementById("location-details").textContent = "The request to get user location timed out.";
+            break;
+        case error.UNKNOWN_ERROR:
+            document.getElementById("location-details").textContent = "An unknown error occurred.";
+            break;
+    }
+}
 
 function findWeather() {
     let txtsearch = document.getElementById("search-bar").value
@@ -52,7 +99,7 @@ function findWeather() {
              L.marker([data.location.lat, data.location.lon]).addTo(map)
             .bindPopup(data.location.name)
             .openPopup();
-            getLocationName(data.location.lat,data.location.lon);
+           
         })
     forecast();
 }
@@ -97,89 +144,6 @@ function forecast() {
 
 }
 
-const map = L.map('map').setView([0, 0], 2); // Start with zoomed-out map
-
-// Add the OpenStreetMap tiles layer (the map imagery)
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-// Check if geolocation is available
-if (navigator.geolocation) {
-    // Request the user's location
-   window.onload= navigator.geolocation.getCurrentPosition(showPositionAuto, showError);
-   
-
-} else {
-    document.getElementById("location-details").textContent = "Geolocation is not supported by this browser.";
-}
-
-// Function to display user's location on the map
-function showPositionAuto(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-
-    // Set the view to the user's location with a suitable zoom level
-    map.setView([lat, lon], 13);
-
-    // Add a marker at the user's location
-    L.marker([lat, lon]).addTo(map)
-        .bindPopup("You are here!")
-        .openPopup();
-
-    // Fetch location name using reverse geocoding from Nominatim
-    getLocationName(lat, lon);
-}
-
-// Function to get location name using Nominatim API
-function getLocationName(lat, lon) {
-    const url = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}&zoom=10&addressdetails=1`;
-
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            const locationDetails = data.address;
-            let locationName = '';
-
-            // Construct location name (you can adjust this as needed)
-            if (locationDetails.city) {
-                locationName += locationDetails.city;
-                window.onload = findWeatherAuto(locationName)
-                window.onload = forecastAuto(locationName)
-                locationName += ','
-            }
-            if (locationDetails.state) {
-                locationName += locationDetails.state + ', ';
-            }
-            if (locationDetails.country) {
-                locationName += locationDetails.country;
-            }
-
-            // Show location name on the page
-            document.getElementById("location-details").textContent = "Location: " + locationName;
-        })
-        .catch(error => {
-            document.getElementById("location-details").textContent = "Unable to retrieve location name.";
-        });
-}
-
-// Handle possible errors
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            document.getElementById("location-details").textContent = "User denied the request for Geolocation.";
-            break;
-        case error.POSITION_UNAVAILABLE:
-            document.getElementById("location-details").textContent = "Location information is unavailable.";
-            break;
-        case error.TIMEOUT:
-            document.getElementById("location-details").textContent = "The request to get user location timed out.";
-            break;
-        case error.UNKNOWN_ERROR:
-            document.getElementById("location-details").textContent = "An unknown error occurred.";
-            break;
-    }
-}
 
 
 
